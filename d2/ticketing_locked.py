12b8379d03bee5df34ee970c528da83aa1d77e1c
Suvam Basak <suvambasak22@iitk.ac.in>
2026-06-20 11:01:20 +0530

    Day 2

---
+++ d2/ticketing_locked.py
@@ -0,0 +1,45 @@
+import time
+from threading import Lock, Thread
+
+lock = Lock()
+
+
+current_balance = 0.0
+
+
+def transfer(amount: float) -> None:
+    "Simulates a transfer of `amount` from the current balance."
+
+    # Validate the transaction
+    time.sleep(0.000001)
+
+    # Get the current balance
+    time.sleep(0.000001)
+
+    with lock:
+        global current_balance
+
+        updated_balance = current_balance + amount
+
+        # Debit the current balance
+        time.sleep(0.000001)
+
+        # Credit the updated balance
+        time.sleep(0.000001)
+        current_balance = updated_balance
+
+
+if __name__ == "__main__":
+    transations: list[Thread] = []
+
+    for _ in range(10000):
+        transation = Thread(target=transfer, args=(1.0,))
+        transations.append(transation)
+
+    for transation in transations:
+        transation.start()
+
+    for transation in transations:
+        transation.join()
+
+    print(f"Final balance value: {current_balance}")
